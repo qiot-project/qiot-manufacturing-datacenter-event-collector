8f3db13a9d9b44bce06aa930a1fe80380cfe67a1
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-10-17 20:28:42 +0200

    fix influxdb maven dependency

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-mvn -B clean package -Pprod oc:build oc:push \
-          -Dquarkus.native.container-build=true \
-          -Dquarkus.container-image.build=true \
-          -Djkube.docker.push.username=${QUAY_USERNAME} \
-          -Djkube.docker.push.password=${QUAY_PASSWORD} 
+#mvn -B clean package -Pprod oc:build oc:push \
+#          -Dquarkus.native.container-build=true \
+#          -Dquarkus.container-image.build=true \
+#          -Djkube.docker.push.username=${QUAY_USERNAME} \
+#          -Djkube.docker.push.password=${QUAY_PASSWORD}
+
+
+
+./mvnw clean package
+docker rmi quay.io/qiotmanufacturing/datacenter-event-collector-jvm:1.0.0.beta1-SNAPSHOT --force
+docker build -f src/main/docker/Dockerfile.jvm -t quay.io/qiotmanufacturing/datacenter-event-collector-jvm:1.0.0.beta1-SNAPSHOT .
+docker push quay.io/qiotmanufacturing/datacenter-event-collector-jvm:1.0.0.beta1-SNAPSHOT 
