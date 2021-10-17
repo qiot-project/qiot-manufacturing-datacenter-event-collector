888213b7a84b44b9a5661599e6742bda72e9d61f
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-10-17 20:11:30 +0200

    Add JIB

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-./mvnw clean package
-docker rmi quay.io/qiotmanufacturing/datacenter-event-collector-jvm:1.0.0-beta1 --force
-docker build -f src/main/docker/Dockerfile.jvm -t quay.io/qiotmanufacturing/datacenter-event-collector-jvm:1.0.0-beta1 .
-docker push quay.io/qiotmanufacturing/datacenter-event-collector-jvm:1.0.0-beta1
+mvn -B clean package -Pprod oc:build oc:push \
+          -Dquarkus.native.container-build=true \
+          -Dquarkus.container-image.build=true \
+          -Djkube.docker.push.username=${QUAY_USERNAME} \
+          -Djkube.docker.push.password=${QUAY_PASSWORD} 
